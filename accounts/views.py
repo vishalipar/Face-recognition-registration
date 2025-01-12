5b3ec2b5b8f694df6bb76061cfed6a3f93b00f19
vishalipar <vishalipar04@gmail.com>
2025-01-12 18:22:55 +0530

    Captures face and show message of registered successfully or already exists.

--- accounts/views.py
+++ accounts/views.py
@@ -6,5 +6,21 @@ from django.core.files.base import ContentFile
 from .models import *
 # Create your views here.
 
+@csrf_exempt
 def register(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        face_image_data = request.POST['face_image']
+        face_image_data = face_image_data.split(",")[1]
+        face_image = ContentFile(base64.b64decode(face_image_data),name=f'{username}_.jpg')
+        try:
+            user = User.objects.create(username = username)
+        except Exception as e:
+            return JsonResponse({
+            'status':'error', 'message':'Username already taken.'
+        })
+        UserImages.objects.create(user=user, face_image = face_image)
+        return JsonResponse({
+            'status':'success', 'message':'User registered successfully.'
+        })
     return render(request, 'register.html')
